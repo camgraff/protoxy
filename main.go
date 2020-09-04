209ceb12f58523daab6446bde36c9c84d1595e05
Cam <graffcameron@gmail.com>
2020-09-03 20:04:42 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hit")
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Error reading body: %v", err)
+			http.Error(w, "can't read body", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Println("Body: ", string(body))
+
+	})
+	log.Fatal(http.ListenAndServe(":7777", nil))
+}
