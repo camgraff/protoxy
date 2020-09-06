966eefc246858e11c5a554a5f6505a624488d1ec
Cam <graffcameron@gmail.com>
2020-09-06 16:57:44 -0500

    Proxy successfully unmarshals json into protobuf

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/camgraff/proto-proxy
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/jhump/protoreflect v1.7.0
+	google.golang.org/protobuf v1.25.0
+)
--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/camgraff/proto-proxy/hello"
+	"github.com/golang/protobuf/proto"
+	"github.com/jhump/protoreflect/desc/protoparse"
+	"github.com/jhump/protoreflect/dynamic"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -18,6 +24,30 @@ func main() {
 		}
 
 		fmt.Println("Body: ", string(body))
+		fmt.Println("Hello", hello.Hello{Hi: "hi there", Number: 12})
+
+		parser := protoparse.Parser{}
+		descriptors, err := parser.ParseFiles("hello.proto")
+		if err != nil {
+			log.Printf("Error parsing proto file: %v", err)
+			return
+		}
+		fmt.Println(descriptors[0])
+		msg := descriptors[0].FindMessage("hello.Hello")
+		if msg == nil {
+			log.Printf("Unable to find message: hello.Hello")
+			return
+		}
+		fmt.Println(msg)
+		fac := dynamic.NewMessageFactoryWithDefaults()
+		helloMsg := fac.NewMessage(msg)
+		helloMsgV2 := proto.MessageV2(helloMsg)
+		fmt.Println(helloMsg)
+		err = protojson.Unmarshal(body, helloMsgV2)
+		if err != nil {
+			log.Printf("Unable to unmarshal into json: %v", err)
+		}
+		fmt.Println(helloMsgV2)
 
 	})
 	log.Fatal(http.ListenAndServe(":7777", nil))
