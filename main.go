06f7460c5c14dbd8f856e49d7043461dc46068bd
Cam <graffcameron@gmail.com>
2020-09-06 19:19:07 -0500

    Implemented proxy to forward protobuf request

--- main.go
+++ main.go
@@ -1,54 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httputil"
 
-	"github.com/camgraff/proto-proxy/hello"
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	"github.com/jhump/protoreflect/desc/protoparse"
 	"github.com/jhump/protoreflect/dynamic"
-	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hit")
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Error reading body: %v", err)
-			http.Error(w, "can't read body", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Println("Body: ", string(body))
-		fmt.Println("Hello", hello.Hello{Hi: "hi there", Number: 12})
-
+	director := func(r *http.Request) {
 		parser := protoparse.Parser{}
 		descriptors, err := parser.ParseFiles("hello.proto")
 		if err != nil {
 			log.Printf("Error parsing proto file: %v", err)
 			return
 		}
-		fmt.Println(descriptors[0])
+
 		msg := descriptors[0].FindMessage("hello.Hello")
 		if msg == nil {
 			log.Printf("Unable to find message: hello.Hello")
 			return
 		}
-		fmt.Println(msg)
-		fac := dynamic.NewMessageFactoryWithDefaults()
-		helloMsg := fac.NewMessage(msg)
-		helloMsgV2 := proto.MessageV2(helloMsg)
-		fmt.Println(helloMsg)
-		err = protojson.Unmarshal(body, helloMsgV2)
+
+		helloMsg := dynamic.NewMessage(msg)
+		err = jsonpb.Unmarshal(r.Body, helloMsg)
 		if err != nil {
 			log.Printf("Unable to unmarshal into json: %v", err)
 		}
-		fmt.Println(helloMsgV2)
 
-	})
+		reqBytes, err := proto.Marshal(helloMsg)
+		if err != nil {
+			log.Printf("Unable to marshal message: %v", err)
+			return
+		}
+		buffer := bytes.NewBuffer(reqBytes)
+		r.Body = ioutil.NopCloser(buffer)
+		r.ContentLength = int64(buffer.Len())
+	}
+	proxy := &httputil.ReverseProxy{Director: director}
+	http.HandleFunc("/", proxy.ServeHTTP)
+
 	log.Fatal(http.ListenAndServe(":7777", nil))
 }
