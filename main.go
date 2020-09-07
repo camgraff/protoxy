0717618250b2e64110325fe8f5d55f6b3660825c
Cam <graffcameron@gmail.com>
2020-09-06 19:53:26 -0500

    Response is successfully converted back to JSON

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"strconv"
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
@@ -43,7 +45,54 @@ func main() {
 		r.Body = ioutil.NopCloser(buffer)
 		r.ContentLength = int64(buffer.Len())
 	}
-	proxy := &httputil.ReverseProxy{Director: director}
+
+	modifyResp := func(r *http.Response) error {
+
+		parser := protoparse.Parser{}
+		descriptors, err := parser.ParseFiles("hello.proto")
+		if err != nil {
+			log.Printf("Error parsing proto file: %v", err)
+			return err
+		}
+
+		msg := descriptors[0].FindMessage("hello.Goodbye")
+		if msg == nil {
+			log.Printf("Unable to find message: hello.Goodbye")
+			return err
+		}
+
+		helloMsg := dynamic.NewMessage(msg)
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Failed to read response body: %v", err)
+			return err
+		}
+		err = r.Body.Close()
+		if err != nil {
+			log.Printf("Error closing body: %v", err)
+		}
+		fmt.Println(string(body))
+		err = proto.Unmarshal(body, helloMsg)
+		if err != nil {
+			log.Printf("Unable to unmarshal into json: %v", err)
+			return err
+		}
+
+		marshaler := jsonpb.Marshaler{}
+		buf := bytes.NewBuffer(nil)
+		err = marshaler.Marshal(buf, helloMsg)
+		fmt.Println(buf)
+		if err != nil {
+			log.Printf("Failed to marshal response: %v", err)
+			return err
+		}
+		r.Body = ioutil.NopCloser(buf)
+		r.ContentLength = int64(buf.Len())
+		r.Header.Set("Content-Length", strconv.Itoa(buf.Len()))
+		return nil
+	}
+
+	proxy := &httputil.ReverseProxy{Director: director, ModifyResponse: modifyResp}
 	http.HandleFunc("/", proxy.ServeHTTP)
 
 	log.Fatal(http.ListenAndServe(":7777", nil))
